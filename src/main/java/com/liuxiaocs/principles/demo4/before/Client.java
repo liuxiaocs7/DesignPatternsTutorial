226ed4497c93b8d5a69695c3b55122a58be35f8d
liuxiao <liuxiao2103@qq.com>
2021-08-17 10:29:42 +0800

    接口隔离原则1

---
+++ src/main/java/com/liuxiaocs/principles/demo4/before/Client.java
@@ -0,0 +1,10 @@
+package com.liuxiaocs.principles.demo4.before;
+
+public class Client {
+    public static void main(String[] args) {
+        HeimaSafetyDoor door = new HeimaSafetyDoor();
+        door.antiTheft();
+        door.fireProof();
+        door.waterProof();
+    }
+}
