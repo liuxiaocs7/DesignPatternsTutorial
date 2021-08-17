277587a580e66339a6f21953eb0202878575dc8e
liuxiao <liuxiao2103@qq.com>
2021-08-17 13:28:04 +0800

    枚举实现单例模式

---
+++ src/main/java/com/liuxiaocs/pattern/singleton/demo6/Client.java
@@ -0,0 +1,9 @@
+package com.liuxiaocs.pattern.singleton.demo6;
+
+public class Client {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.INSTANCE;
+        Singleton instance1 = Singleton.INSTANCE;
+        System.out.println(instance == instance1);
+    }
+}
